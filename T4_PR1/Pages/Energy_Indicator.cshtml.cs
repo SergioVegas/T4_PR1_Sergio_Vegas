90330fa76a769aa3f678670a1302611adca46300
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2025-02-27 13:16:38 +0100

    Tots el documents inicials per treballar estan al programa. #11

---
+++ T4_PR1/Pages/Energy_Indicator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace T4_PR1.Pages
+{
+    public class Energy_IndicatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ T4_PR1/Pages/Water_Consumption.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace T4_PR1.Pages
+{
+    public class Water_ConsumptionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
