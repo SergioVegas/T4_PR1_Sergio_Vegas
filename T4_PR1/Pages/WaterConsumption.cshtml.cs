502c55df746a28a90688e574f448f20839e858f2
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-03 19:45:18 +0100

    Primera consulta feta

--- T4_PR1/Pages/WaterConsumption.cshtml.cs
+++ T4_PR1/Pages/WaterConsumption.cshtml.cs
@@ -21,6 +21,11 @@ namespace T4_PR1.Pages
         public int TotalPages { get; set; } 
         public WaterConsumption HeaderRow { get; set; } = new WaterConsumption();
 
+        // Propietats per less estadistiques
+        public List<WaterConsumption> Top10Municipalities { get; set; } = new List<WaterConsumption>();
+        public List<(string Comarca, double AverageConsumption)> AverageConsumptionByRegion { get; set; } = new List<(string, double)>();
+        public List<WaterConsumption> SuspiciousConsumptionMunicipalities { get; set; } = new List<WaterConsumption>();
+        public List<WaterConsumption> IncreasingTrendMunicipalities { get; set; } = new List<WaterConsumption>();
         public void OnGet(int? pageNumber)
         {
             if (pageNumber.HasValue)
@@ -42,12 +47,28 @@ namespace T4_PR1.Pages
 
                 
                 HeaderRow = CurrentPageWaterConsumptions.FirstOrDefault() ?? new WaterConsumption(); //  Assegurem que sempre hi hagui una capçalera, agafant la primera linea del archiu o tornant una nova instancia
+
+                ConsumptionWaterAnalisis();
             }
             catch (Exception ex)
             {
                 _logger.LogError(ex, "Error al leer el archivo CSV.");
                 ModelState.AddModelError(string.Empty, "Ocurrió un error al cargar los datos. Por favor, inténtalo de nuevo.");
             }
+        }
+        public void ConsumptionWaterAnalisis()
+        {
+            if (WaterConsumptions == null || WaterConsumptions.Count == 0)
+            {
+                return; 
+            }
+
+            int mostRecentYear = WaterConsumptions.Max(w => w.Year);
+            Top10Municipalities = WaterConsumptions
+                .Where(w=>w.Year == mostRecentYear)
+                .OrderByDescending(w=>w.Total)
+                .Take(10)
+                .ToList();
         }
     }
 }
