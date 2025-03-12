26c4d8fcf08cca941e59008255eb884e2a697c24
SergioVegas <sergio.vegas.7e8@itb.cat>
2025-03-13 00:12:36 +0100

    Se me olvido algo

--- T4_PR1/Model/EnergyClasses/WindSystem.cs
+++ T4_PR1/Model/EnergyClasses/WindSystem.cs
@@ -13,17 +13,17 @@ namespace T4._PR1._Practica_1.EnegyClass
         [Required, Range(5, double.MaxValue, ErrorMessage = "La velocitat del vent no pot ser menor a 5, torna a introduir un número.")]
         public double WindVelocity { set; get; }
 
-        //Constructor amb més carga logica
+        //Constructor amb més càrrega lògica
         public WindSystem(double windVelocity, DateTime date, TypeEnergy name, double costEnergy, double priceEnergy, double rati, double generatedEnergy, double totalCost, double totalPrice) : base(date, name, costEnergy, priceEnergy, rati, generatedEnergy, totalCost, totalPrice)
         {
             WindVelocity = windVelocity;
         }
         public WindSystem() { }
-      
-        
-        public override double CalculateEnergy( )
-        {
-           return Math.Round(Math.Pow(WindVelocity, 3) * Rati); 
-        }
+
+        /// <summary>
+        /// Calcula la energia generda d'un sitema d'enegia eolica
+        /// </summary>
+        /// <returns>Energia calculada</returns>
+        public override double CalculateEnergy( )=> Math.Round(Math.Pow(WindVelocity, 3) * Rati); 
     }
 }
